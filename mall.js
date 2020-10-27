61cd41a3cdd35c70ceb1326ae6942b09f7f2bb8a
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-10-27 15:14:29 +0900

    Update mall.js
    by myself

--- mall.js
+++ mall.js
@@ -1,1 +1,24 @@
 "use strict.";
+
+class Clothes {
+  constructor(gender, size) {
+    this.gender = gender;
+    this.size = size;
+  }
+}
+const items = [
+  new Clothes("female", "large"),
+  new Clothes("man", "small"),
+  new Clothes("man", "large"),
+  new Clothes("man", "large"),
+  new Clothes("female", "large"),
+  new Clothes("male", "large"),
+  new Clothes("male", "large"),
+  new Clothes("female", "small"),
+  new Clothes("female", "large"),
+  new Clothes("man", "small"),
+];
+
+function Click{
+    
+}
